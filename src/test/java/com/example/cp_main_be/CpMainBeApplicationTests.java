435e9ab91f40de2a11419e96e6def2c33e0ea637
lejuho <zedd1324@gmail.com>
2025-06-29 22:26:39 +0900

    프로젝트 초기화

---
+++ src/test/java/com/example/cp_main_be/CpMainBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cp_main_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpMainBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
