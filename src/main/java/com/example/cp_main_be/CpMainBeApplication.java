a08c85c19f0d57f3673eacd6f11d15e5e4017c08
GitHub Action <action@github.com>
2025-07-02 15:12:39 +0000

    🎨 Auto-format code with Spotless [skip ci]

--- src/main/java/com/example/cp_main_be/CpMainBeApplication.java
+++ src/main/java/com/example/cp_main_be/CpMainBeApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CpMainBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CpMainBeApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(CpMainBeApplication.class, args);
+  }
 }
