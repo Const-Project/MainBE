78abe13cf82d63c49c7b69499ceff2ec67e90d39
lejuho <52308082+lejuho@users.noreply.github.com>
2025-07-15 12:45:04 +0000

    🎨 [BOT] Apply Spotless code style

--- src/main/java/com/example/cp_main_be/avatar/presentation/AvatarController.java
+++ src/main/java/com/example/cp_main_be/avatar/presentation/AvatarController.java
@@ -1,6 +1,5 @@
 package com.example.cp_main_be.avatar.presentation;
 
-
 import com.example.cp_main_be.avatar.dto.response.AvatarResponse;
 import com.example.cp_main_be.avatar.service.AvatarService;
 import lombok.RequiredArgsConstructor;
@@ -14,11 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class AvatarController {
 
-    private final AvatarService avatarService;
+  private final AvatarService avatarService;
 
-    @GetMapping("/register/avatars")
-    public ResponseEntity<AvatarResponse> selectableAvatars() {
-        AvatarResponse avatarResponse = new AvatarResponse(avatarService.getAllAvatar());
-        return ResponseEntity.ok(avatarResponse);
-    }
+  @GetMapping("/register/avatars")
+  public ResponseEntity<AvatarResponse> selectableAvatars() {
+    AvatarResponse avatarResponse = new AvatarResponse(avatarService.getAllAvatar());
+    return ResponseEntity.ok(avatarResponse);
+  }
 }
