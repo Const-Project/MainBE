71181bd32d503a544a8ee92a3faa2690ebade129
itaekyung <taeyun1411@gmail.com>
2025-08-05 04:38:56 +0900

    chore: spotless 적용

--- src/main/java/com/example/cp_main_be/domain/image/ImageUploader.java
+++ src/main/java/com/example/cp_main_be/domain/image/ImageUploader.java
@@ -4,5 +4,5 @@ import org.springframework.web.multipart.MultipartFile;
 
 public interface ImageUploader {
 
-    String upload(MultipartFile file, String path);
+  String upload(MultipartFile file, String path);
 }
