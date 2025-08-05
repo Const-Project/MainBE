0ce2364da96fc26e1f4d11642e59f8ee595c6557
itaekyung <taeyun1411@gmail.com>
2025-08-06 06:23:58 +0900

    feat: avatarPost 엔티티 초기 세팅

---
+++ src/main/java/com/example/cp_main_be/domain/social/avatarpost/presentation/AvatarPostController.java
@@ -0,0 +1,14 @@
+package com.example.cp_main_be.domain.social.avatarpost.presentation;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/avatar-posts")
+public class AvatarPostController {
+
+  //    @GetMapping("/{postId}")
+  //    public ResponseEntity<ApiResponse<>>
+}
