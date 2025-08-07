1d58e79a0b15e1629595fbdbfe44205460b9682c
c5ln <cjstjdgh0209@gmail.com>
2025-08-07 20:04:13 +0900

    Feat : 미션 기능 구현을 위한 Entity 구현

---
+++ src/main/java/com/example/cp_main_be/domain/daily_mission_masters/MissionType.java
@@ -0,0 +1,5 @@
+package com.example.cp_main_be.domain.daily_mission_masters;
+
+public enum MissionType {
+    PHOTO,QUIZ,DIARY
+}
---
+++ src/main/java/com/example/cp_main_be/domain/daily_mission_masters/domain/DailyMissionMasters.java
@@ -0,0 +1,46 @@
+package com.example.cp_main_be.domain.daily_mission_masters.domain;
+
+
+import com.example.cp_main_be.domain.daily_mission_masters.MissionType;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class DailyMissionMasters {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mission_master_id")
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "mission_type")
+    private MissionType missionType;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "content")
+    private String content;
+
+    @Column(name = "reward_points")
+    private Long rewardPoints;
+
+    @Column(name = "is_active")
+    private Boolean isActive;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
