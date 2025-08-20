c240914f6d8393b832ef08603c6521bf158a3395
lejuho <zedd1324@gmail.com>
2025-08-20 19:39:41 +0900

    fix : 누락된 코드 보강

---
+++ src/main/java/com/example/cp_main_be/domain/keyword/dto/response/KeywordResponse.java
@@ -0,0 +1,5 @@
+package com.example.cp_main_be.domain.keyword.dto.response;
+
+import java.time.LocalDateTime;
+
+public record KeywordResponse(LocalDateTime date, String keyword) {}
---
+++ src/main/java/com/example/cp_main_be/domain/keyword/dto/response/TodayKeywordResponse.java
@@ -0,0 +1,3 @@
+package com.example.cp_main_be.domain.keyword.dto.response;
+
+public record TodayKeywordResponse(String keyword) {}
