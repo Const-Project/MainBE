caf0f37d956cc46ac6d798328c6d42ccad3cbf77
lejuho <52308082+lejuho@users.noreply.github.com>
2025-08-21 12:15:14 +0900

    Chore: 성호 브랜치에 남은 커밋 적용 (#41)
    * Revert "Feat/mission and admin api (#35)"
    
    This reverts commit 7280c0990340c43d49232f6f4393c8d5928e90c4.
    
    * Fix : 작은 논리적 버그들 수정
    
    * chore : 충돌 해결
    
    ---------
    
    Co-authored-by: c5ln <cjstjdgh0209@gmail.com>

--- src/main/java/com/example/cp_main_be/domain/social/diaryimage/domain/DiaryImageRepository.java
+++ src/main/java/com/example/cp_main_be/domain/social/diaryimage/domain/DiaryImageRepository.java
@@ -2,6 +2,4 @@ package com.example.cp_main_be.domain.social.diaryimage.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface DiaryImageRepository
-    extends JpaRepository<
-        com.example.cp_main_be.domain.social.diaryimage.domain.DiaryImage, Long> {}
+public interface DiaryImageRepository extends JpaRepository<DiaryImage, Long> {}
