291415c8e3198ef33174d4a73122fd884a6bf619
itaekyung <taeyun1411@gmail.com>
2025-08-22 13:16:32 +0900

    spotless apply

--- src/main/java/com/example/cp_main_be/domain/social/like/domain/repository/LikeRepository.java
+++ src/main/java/com/example/cp_main_be/domain/social/like/domain/repository/LikeRepository.java
@@ -2,9 +2,8 @@ package com.example.cp_main_be.domain.social.like.domain.repository;
 
 import com.example.cp_main_be.domain.member.user.domain.User;
 import com.example.cp_main_be.domain.social.like.domain.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
   Optional<Like> findByUserAndTargetIdAndTargetType(User user, Long targetId, String targetType);
