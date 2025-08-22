9c7d3fce9fd5522cd24c3187b5624021710e64e5
itaekyung <taeyun1411@gmail.com>
2025-08-22 11:31:06 +0900

    spotless apply

--- src/main/java/com/example/cp_main_be/domain/social/diary/domain/Repository/DiaryRepository.java
+++ src/main/java/com/example/cp_main_be/domain/social/diary/domain/Repository/DiaryRepository.java
@@ -2,10 +2,9 @@ package com.example.cp_main_be.domain.social.diary.domain.repository;
 
 import com.example.cp_main_be.domain.member.user.domain.User;
 import com.example.cp_main_be.domain.social.diary.domain.Diary;
+import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
   List<Diary> findByUserInAndIsPublicIsTrue(List<User> users, Pageable pageable);
