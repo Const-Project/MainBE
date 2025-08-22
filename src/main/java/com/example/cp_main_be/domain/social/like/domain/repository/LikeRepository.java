65467e41a88239c2a6c955db29da47d654e95b4d
itaekyung <taeyun1411@gmail.com>
2025-08-22 12:59:33 +0900

    refactor: LikeRepository 정리

--- src/main/java/com/example/cp_main_be/domain/social/like/domain/repository/LikeRepository.java
+++ src/main/java/com/example/cp_main_be/domain/social/like/domain/repository/LikeRepository.java
@@ -1,17 +1,17 @@
 package com.example.cp_main_be.domain.social.like.domain.repository;
 
 import com.example.cp_main_be.domain.member.user.domain.User;
-import com.example.cp_main_be.domain.social.avatarpost.domain.AvatarPost;
 import com.example.cp_main_be.domain.social.like.domain.Like;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface LikeRepository extends JpaRepository<Like, Long> {
   Optional<Like> findByUserAndTargetIdAndTargetType(User user, Long targetId, String targetType);
 
   boolean existsByUserAndTargetIdAndTargetType(User user, Long targetId, String targetType);
 
-  long countByAvatarPost(AvatarPost post);
+  //  long countByAvatarPost(AvatarPost post);
 
-  Optional<Like> findByUserAndAvatarPost(User currentUser, AvatarPost post);
+  //  Optional<Like> findByUserAndAvatarPost(User currentUser, AvatarPost post);
 }
