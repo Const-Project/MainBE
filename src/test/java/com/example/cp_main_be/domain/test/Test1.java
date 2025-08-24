009eb6d91d227d22ea338f666ee3b40f81f53499
이태경 <161710171+xoruddl@users.noreply.github.com>
2025-08-24 13:15:52 +0900

    feat: 임시 테스트 코드 작성 (무조건 통과) (#52)

---
+++ src/test/java/com/example/cp_main_be/domain/test/Test1.java
@@ -0,0 +1,20 @@
+package com.example.cp_main_be.domain.test;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class Test1 {
+
+  @Test
+  public void hi() throws Exception {
+    // given
+
+    // when
+
+    // then
+    Assertions.assertThat(true).isTrue();
+  }
+}
