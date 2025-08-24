6570c49e05c6ba81367aa2d01e53613bdb0db0fb
lejuho <52308082+lejuho@users.noreply.github.com>
2025-08-24 21:35:29 +0900

    Refactor/lejuhooo (#78)
    * 수정
    
    * hook 적용
    
    * spotless

--- src/main/java/com/example/cp_main_be/domain/member/auth/dto/response/AnonymousRegistrationResponse.java
+++ src/main/java/com/example/cp_main_be/domain/member/auth/dto/response/AnonymousRegistrationResponse.java
@@ -9,6 +9,7 @@ public class AnonymousRegistrationResponse {
 
   private final String accessToken;
   private final String refreshToken;
+  private final Long userId;
   private final String nickname; // [추가] 생성된 닉네임
   private final boolean isNewUser;
 }
