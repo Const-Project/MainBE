06d837929e8c3d1e8abe31c7a3844f527f070257
itaekyung <taeyun1411@gmail.com>
2025-08-24 22:05:00 +0900

    spotless apply

--- src/main/java/com/example/cp_main_be/domain/social/feed/presentation/FeedController.java
+++ src/main/java/com/example/cp_main_be/domain/social/feed/presentation/FeedController.java
@@ -6,7 +6,6 @@ import com.example.cp_main_be.domain.social.feed.service.FeedService;
 import com.example.cp_main_be.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -14,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
