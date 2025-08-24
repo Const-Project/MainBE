32865b1896454a66894b905ec6e49c8d7685f4e5
c5ln <cjstjdgh0209@gmail.com>
2025-08-24 17:16:03 +0900

    Refactor : 팔오우, 팔로잉 목록 조회 DTO 수정

---
+++ src/main/java/com/example/cp_main_be/domain/social/follow/dto/FollowResponseDTO.java
@@ -0,0 +1,16 @@
+package com.example.cp_main_be.domain.social.follow.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FollowResponseDTO {
+    private Long userId;
+    private String username;
+    private String userImageUrl;
+}
