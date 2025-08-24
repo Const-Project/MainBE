bc5e08ff06be2d31db85502a9ef2722489a715b9
lejuho <52308082+lejuho@users.noreply.github.com>
2025-08-24 17:39:46 +0900

    cicd 체크 (#61)
    * cicd 체크
    
    * spotless
    
    * 테스트 삭제

--- src/main/java/com/example/cp_main_be/domain/admin/presentation/AdminController.java
+++ src/main/java/com/example/cp_main_be/domain/admin/presentation/AdminController.java
@@ -18,7 +18,6 @@ import com.example.cp_main_be.domain.admin.dto.AdminResponseDTO;
 import com.example.cp_main_be.domain.admin.service.AdminService;
 import com.example.cp_main_be.domain.delivery.domain.DeliveryPlant;
 import com.example.cp_main_be.domain.delivery.dto.request.DeliveryPlantRequest;
-import com.example.cp_main_be.domain.garden.plant_masters.domain.PlantMasters;
 import com.example.cp_main_be.domain.member.user.domain.User;
 import com.example.cp_main_be.domain.mission.daily_keywords.domain.DailyKeywords;
 import com.example.cp_main_be.domain.mission.daily_mission_master.domain.DailyMissionMaster;
@@ -106,25 +105,26 @@ public class AdminController {
     QuizOptions quizOptions = adminService.createQuizOption(requestDTO);
     return ResponseEntity.ok(ApiResponse.success(quizOptions));
   }
-//
-//  @PostMapping("/avatar-master")
-//  @Operation(summary = "새 아바타 마스터 등록 API")
-//  public ResponseEntity<ApiResponse<AdminResponseDTO.PlantMasterResDTO>> createNewPlant(
-//      AdminRequestDTO.CreatePlantMasterRequestDTO requestDTO) {
-//    PlantMasters plantMaster = adminService.createNewPlant(requestDTO);
-//    AdminResponseDTO.PlantMasterResDTO result = PlantMasters.toPlantMasterResDTO(plantMaster);
-//    return ResponseEntity.ok(ApiResponse.success(result));
-//  }
-//
-//  @PutMapping("/plants/{avatar-master-id}")
-//  @Operation(summary = "아바타 마스터 정보 수정 API")
-//  public ResponseEntity<ApiResponse<AdminResponseDTO.PlantMasterResDTO>> updatePlantMasters(
-//      @PathVariable(name = "avatar-master-id") Long plantId,
-//      AdminRequestDTO.UpdatePlantMasterRequestDTO requestDTO) {
-//    PlantMasters plantMaster = adminService.updatePlantMasters(plantId, requestDTO);
-//    AdminResponseDTO.PlantMasterResDTO result = PlantMasters.toPlantMasterResDTO(plantMaster);
-//    return ResponseEntity.ok(ApiResponse.success(result));
-//  }
+
+  //
+  //  @PostMapping("/avatar-master")
+  //  @Operation(summary = "새 아바타 마스터 등록 API")
+  //  public ResponseEntity<ApiResponse<AdminResponseDTO.PlantMasterResDTO>> createNewPlant(
+  //      AdminRequestDTO.CreatePlantMasterRequestDTO requestDTO) {
+  //    PlantMasters plantMaster = adminService.createNewPlant(requestDTO);
+  //    AdminResponseDTO.PlantMasterResDTO result = PlantMasters.toPlantMasterResDTO(plantMaster);
+  //    return ResponseEntity.ok(ApiResponse.success(result));
+  //  }
+  //
+  //  @PutMapping("/plants/{avatar-master-id}")
+  //  @Operation(summary = "아바타 마스터 정보 수정 API")
+  //  public ResponseEntity<ApiResponse<AdminResponseDTO.PlantMasterResDTO>> updatePlantMasters(
+  //      @PathVariable(name = "avatar-master-id") Long plantId,
+  //      AdminRequestDTO.UpdatePlantMasterRequestDTO requestDTO) {
+  //    PlantMasters plantMaster = adminService.updatePlantMasters(plantId, requestDTO);
+  //    AdminResponseDTO.PlantMasterResDTO result = PlantMasters.toPlantMasterResDTO(plantMaster);
+  //    return ResponseEntity.ok(ApiResponse.success(result));
+  //  }
 
   @GetMapping("/reports")
   @Operation(summary = "신고 목록 조회 API")
@@ -168,8 +168,9 @@ public class AdminController {
 
   @PostMapping(value = "/plant/delivery/", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
   @Operation(summary = "택배로 받을 식물 추가")
-  public ResponseEntity<ApiResponse<DeliveryPlant>> addDeliveryPlant(@RequestParam MultipartFile file, @RequestBody DeliveryPlantRequest request) {
-      DeliveryPlant deliveryPlant = adminService.addDeliveryPlant(file,request);
-      return ResponseEntity.ok(ApiResponse.success(deliveryPlant));
+  public ResponseEntity<ApiResponse<DeliveryPlant>> addDeliveryPlant(
+      @RequestParam MultipartFile file, @RequestBody DeliveryPlantRequest request) {
+    DeliveryPlant deliveryPlant = adminService.addDeliveryPlant(file, request);
+    return ResponseEntity.ok(ApiResponse.success(deliveryPlant));
   }
 }
--- src/main/java/com/example/cp_main_be/domain/member/user/presentation/UserController.java
+++ src/main/java/com/example/cp_main_be/domain/member/user/presentation/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/users")
 @Tag(name = "유저 API", description = "회원 관련 기능을 제공합니다.")
 public class UserController {
 
@@ -35,14 +35,14 @@ public class UserController {
   //    }
 
   @Operation(summary = "회원 탈퇴", description = "서비스에서 탈퇴합니다")
-  @DeleteMapping("/users/me")
+  @DeleteMapping("/me")
   public ResponseEntity<ApiResponse<Void>> deleteUser(@AuthenticationPrincipal User user) {
     userService.deleteUser(user.getId());
     return ResponseEntity.ok(ApiResponse.success(null));
   }
 
   @Operation(summary = "아바타 수정", description = "새로운 아바타 정보로 업데이트합니다")
-  @PatchMapping("/users/me/{avatarId}")
+  @PatchMapping("/me/{avatarId}")
   public ResponseEntity<ApiResponse<Void>> updateAvatar(
       @AuthenticationPrincipal User user,
       @RequestBody @Valid AvatarChangeRequest request,
@@ -53,7 +53,7 @@ public class UserController {
   }
 
   @Operation(summary = "유저 닉네임 수정", description = "유저 닉네임을 수정합니다")
-  @PatchMapping("/users/me/nickname")
+  @PatchMapping("/me/nickname")
   public ResponseEntity<ApiResponse<Void>> updateNickname(
       @AuthenticationPrincipal User user, @RequestBody @Valid NicknameChangeRequest request) {
     userService.updateNickname(user, request.getNewNickname());
@@ -61,7 +61,7 @@ public class UserController {
   }
 
   @Operation(summary = "내 정보 조회", description = "내 정보를 조회합니다")
-  @GetMapping("/users/me")
+  @GetMapping("/me")
   public ResponseEntity<ApiResponse<UserRegisterResponse>> getMyInfo(
       @AuthenticationPrincipal User user) {
     // SecurityContextHolder에서 현재 인증된 사용자(UUID)를 가져옴
@@ -78,15 +78,19 @@ public class UserController {
     UserRegisterResponse.LevelStatusResponseDTO levelStatusResponseDTO = userService.getLevel(user);
     return ResponseEntity.ok(ApiResponse.success(levelStatusResponseDTO));
   }
+
   @Operation(summary = "유저 정보 조회", description = "유저 정보를 조회합니다")
   @GetMapping("/{userId}")
   public ResponseEntity<ApiResponse<UserRegisterResponse>> getUserInfo(
-          @PathParam("userId") Long userId) {
+      @PathParam("userId") Long userId) {
     // SecurityContextHolder에서 현재 인증된 사용자(UUID)를 가져옴
-    User user = userRepository.findById(userId).orElseThrow(()->new UserNotFoundException("해당 유저를 찾을 수 없습니다."));
+    User user =
+        userRepository
+            .findById(userId)
+            .orElseThrow(() -> new UserNotFoundException("해당 유저를 찾을 수 없습니다."));
     UserRegisterResponse userRegisterResponse =
-            new UserRegisterResponse(
-                    user.getId(), user.getNickname(), user.getUuid(), null, null); // 토큰은 응답에 포함하지 않음
+        new UserRegisterResponse(
+            user.getId(), user.getNickname(), user.getUuid(), null, null); // 토큰은 응답에 포함하지 않음
     return ResponseEntity.ok(ApiResponse.success(userRegisterResponse));
   }
 }
