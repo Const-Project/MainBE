b4861b82aafbe4ef2fdfdd1f51158a7c847e0f3e
c5ln <70241022+c5ln@users.noreply.github.com>
2025-08-24 17:20:30 +0900

    Refactor : 팔로우, 팔로잉 목록 조회 DTO 수정 (#59)

--- src/main/java/com/example/cp_main_be/domain/social/follow/presentation/FollowController.java
+++ src/main/java/com/example/cp_main_be/domain/social/follow/presentation/FollowController.java
@@ -2,6 +2,7 @@ package com.example.cp_main_be.domain.social.follow.presentation;
 
 import com.example.cp_main_be.domain.member.user.domain.User;
 // UserService import 추가
+import com.example.cp_main_be.domain.social.follow.dto.FollowResponseDTO;
 import com.example.cp_main_be.domain.social.follow.service.FollowService;
 import com.example.cp_main_be.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -36,17 +37,17 @@ public class FollowController {
     return ResponseEntity.ok(ApiResponse.success(null));
   }
 
-  @Operation(summary = "내 팔로워 조회", description = "나를 팔로우하는 유저 목록을 조회합니다")
+  @Operation(summary = "팔로워 조회", description = "팔로우하는 유저 목록을 조회합니다")
   @GetMapping("/{userId}/followers")
-  public ResponseEntity<ApiResponse<List<User>>> getFollowers(@PathVariable Long userId) {
-    List<User> followers = followService.getFollowers(userId);
-    return ResponseEntity.ok(ApiResponse.success(followers));
+  public ResponseEntity<ApiResponse<List<FollowResponseDTO>>> getFollowers(@PathVariable Long userId) {
+    List<FollowResponseDTO> followResponseDTOS = followService.getFollowers(userId);
+    return ResponseEntity.ok(ApiResponse.success(followResponseDTOS));
   }
 
-  @Operation(summary = "내 팔로잉 조회", description = "내가 팔로우하는 유저 목록을 조회합니다")
+  @Operation(summary = "팔로잉 조회", description = "팔로우하는 유저 목록을 조회합니다")
   @GetMapping("/{userId}/following")
-  public ResponseEntity<ApiResponse<List<User>>> getFollowing(@PathVariable Long userId) {
-    List<User> following = followService.getFollowing(userId);
-    return ResponseEntity.ok(ApiResponse.success(following));
+  public ResponseEntity<ApiResponse<List<FollowResponseDTO>>> getFollowing(@PathVariable Long userId) {
+    List<FollowResponseDTO> followResponseDTOS = followService.getFollowing(userId);
+    return ResponseEntity.ok(ApiResponse.success(followResponseDTOS));
   }
 }
