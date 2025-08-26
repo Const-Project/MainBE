a45ba118adcf8fd5f73d089ac53209b51d16a7f8
c5ln <cjstjdgh0209@gmail.com>
2025-08-26 22:42:31 +0900

    코드 정렬

--- src/main/java/com/example/cp_main_be/domain/realquiz/presentation/RealQuizController.java
+++ src/main/java/com/example/cp_main_be/domain/realquiz/presentation/RealQuizController.java
@@ -1,6 +1,5 @@
 package com.example.cp_main_be.domain.realquiz.presentation;
 
-
 import com.example.cp_main_be.domain.member.user.domain.User;
 import com.example.cp_main_be.domain.mission.quiz.enums.QuizType;
 import com.example.cp_main_be.domain.realquiz.dto.request.RealQuizAnswerRequestDTO;
@@ -11,14 +10,11 @@ import com.example.cp_main_be.domain.realquiz.dto.response.RealQuizResponseDTO;
 import com.example.cp_main_be.domain.realquiz.dto.response.UserQuizCreateResponseDTO;
 import com.example.cp_main_be.domain.realquiz.service.RealQuizService;
 import com.example.cp_main_be.global.common.ApiResponse;
-import com.google.protobuf.Api;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/realQuiz")
@@ -26,34 +22,38 @@ import java.util.List;
 @Tag(name = "퀴즈 컨트롤러", description = "퀴즈 관련 기능을 제공합니다.")
 public class RealQuizController {
 
-    private final RealQuizService realQuizService;
+  private final RealQuizService realQuizService;
 
-    @PostMapping("/create")
-    @Operation(summary = "퀴즈 생성 API")
-    public ApiResponse<RealQuizResponseDTO> createRealQuiz(@RequestBody RealQuizCreateRequestDTO requestDTO) {
-        RealQuizResponseDTO responseDTO = realQuizService.createRealQuiz(requestDTO);
-        return ApiResponse.success(responseDTO);
-    }
+  @PostMapping("/create")
+  @Operation(summary = "퀴즈 생성 API")
+  public ApiResponse<RealQuizResponseDTO> createRealQuiz(
+      @RequestBody RealQuizCreateRequestDTO requestDTO) {
+    RealQuizResponseDTO responseDTO = realQuizService.createRealQuiz(requestDTO);
+    return ApiResponse.success(responseDTO);
+  }
 
-    @PostMapping("/assign")
-    @Operation(summary = "유저에게 퀴즈 수동 할당 API")
-    public ApiResponse<UserQuizCreateResponseDTO> createUserQuiz(@RequestBody UserQuizCreateRequestDTO requestDTO) {
-        UserQuizCreateResponseDTO responseDTO = realQuizService.createUserQuiz(requestDTO.getUserId(), requestDTO.getQuizId());
-        return ApiResponse.success(responseDTO);
-    }
+  @PostMapping("/assign")
+  @Operation(summary = "유저에게 퀴즈 수동 할당 API")
+  public ApiResponse<UserQuizCreateResponseDTO> createUserQuiz(
+      @RequestBody UserQuizCreateRequestDTO requestDTO) {
+    UserQuizCreateResponseDTO responseDTO =
+        realQuizService.createUserQuiz(requestDTO.getUserId(), requestDTO.getQuizId());
+    return ApiResponse.success(responseDTO);
+  }
 
-    @GetMapping
-    @Operation(summary = "자신의 퀴즈 조회 API")
-    public ApiResponse<RealQuizResponseDTO> getRealQuiz(@AuthenticationPrincipal User user, @RequestParam QuizType quizType) {
-        RealQuizResponseDTO responseDTO = realQuizService.getRealQuiz(user,quizType);
-        return ApiResponse.success(responseDTO);
-    }
+  @GetMapping
+  @Operation(summary = "자신의 퀴즈 조회 API")
+  public ApiResponse<RealQuizResponseDTO> getRealQuiz(
+      @AuthenticationPrincipal User user, @RequestParam QuizType quizType) {
+    RealQuizResponseDTO responseDTO = realQuizService.getRealQuiz(user, quizType);
+    return ApiResponse.success(responseDTO);
+  }
 
-    @PostMapping("/{quizId}/answer")
-    @Operation(summary = "퀴즈 정답 제출 API")
-    public ApiResponse<RealQuizAnswerResponseDTO> getRealQuizAnswer(@PathVariable Long quizId, @RequestBody RealQuizAnswerRequestDTO requestDTO)
-    {
-        RealQuizAnswerResponseDTO responseDTO = realQuizService.getRealQuizAnswer(quizId, requestDTO);
-        return ApiResponse.success(responseDTO);
-    }
+  @PostMapping("/{quizId}/answer")
+  @Operation(summary = "퀴즈 정답 제출 API")
+  public ApiResponse<RealQuizAnswerResponseDTO> getRealQuizAnswer(
+      @PathVariable Long quizId, @RequestBody RealQuizAnswerRequestDTO requestDTO) {
+    RealQuizAnswerResponseDTO responseDTO = realQuizService.getRealQuizAnswer(quizId, requestDTO);
+    return ApiResponse.success(responseDTO);
+  }
 }
--- src/main/java/com/example/cp_main_be/domain/realquiz/repository/RealQuizOptionRepository.java
+++ src/main/java/com/example/cp_main_be/domain/realquiz/repository/RealQuizOptionRepository.java
@@ -2,10 +2,9 @@ package com.example.cp_main_be.domain.realquiz.repository;
 
 import com.example.cp_main_be.domain.realquiz.RealQuiz;
 import com.example.cp_main_be.domain.realquiz.RealQuizOption;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface RealQuizOptionRepository extends JpaRepository<RealQuizOption, Long> {
-    List<RealQuizOption> findAllByRealQuiz(RealQuiz realQuiz);
+  List<RealQuizOption> findAllByRealQuiz(RealQuiz realQuiz);
 }
