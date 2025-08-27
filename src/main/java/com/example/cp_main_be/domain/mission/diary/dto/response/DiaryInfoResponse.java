2827d674c705f729453320fb1a130c6cfc67fc4f
itaekyung <taeyun1411@gmail.com>
2025-08-27 20:56:17 +0900

    refactor: DiaryInfoResponse 수정

--- src/main/java/com/example/cp_main_be/domain/mission/diary/dto/response/DiaryInfoResponse.java
+++ src/main/java/com/example/cp_main_be/domain/mission/diary/dto/response/DiaryInfoResponse.java
@@ -7,14 +7,14 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 public record DiaryInfoResponse(
-    Long id,
-    Long writerId,
-    String writerName,
-    String profileImageUrl,
-    String title,
-    String content,
-    String imageUrl,
-    boolean isLiked,
+    Long id,  // 일기 id
+    Long writerId,  // 작성자 id
+    String writerName,  // 작성자 이름
+    String profileImageUrl,   // 작성자 프로필 이미지 url
+    String title, // 일기 제목
+    String content,   // 일기 내용
+    String imageUrl,  // 일기 이미지
+    boolean isLiked,  //
     int likeCount,
     int commentCount,
     List<CommentResponseDTO> comments,
@@ -32,11 +32,11 @@ public record DiaryInfoResponse(
     return new DiaryInfoResponse(
         diary.getId(),
         diary.getUser().getId(),
+        writerName,
+        profileImageUrl,
         diary.getTitle(),
         diary.getContent(),
         imageUrl,
-        writerName,
-        profileImageUrl,
         isLiked,
         diary.getLikeCount(),
         commentDTOs.size(),
