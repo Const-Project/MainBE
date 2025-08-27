cfcbfc2ad7a9111c37e6087fa9c846d2ddef19be
c5ln <cjstjdgh0209@gmail.com>
2025-08-27 10:36:36 +0900

    Fix : NPE 해결 진짜 완료

--- src/main/java/com/example/cp_main_be/domain/home/service/HomeService.java
+++ src/main/java/com/example/cp_main_be/domain/home/service/HomeService.java
@@ -166,7 +166,7 @@ public class HomeService {
     LocalDateTime startOfDay = LocalDate.now().atStartOfDay();
     LocalDateTime endOfDay = LocalDate.now().atTime(23, 59, 59);
 
-    Diary diary = diaryService.findMyDiaries(user).stream().findFirst().orElse(null);
+    Diary diary = diaryRepository.findTodayDiaryByUser(user, startOfDay,endOfDay).stream().findFirst().orElse(null);
     if (diary != null && diary.getCreatedAt().isAfter(LocalDate.now().atStartOfDay())) {
       isDiaryCompleted = true;
     }
