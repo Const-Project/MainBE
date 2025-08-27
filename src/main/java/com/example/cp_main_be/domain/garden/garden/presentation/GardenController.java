bc433fb2ea673c44eaf77327aebceb96ac961c7c
lejuho <zedd1324@gmail.com>
2025-08-28 05:18:31 +0900

    응애4

--- src/main/java/com/example/cp_main_be/domain/garden/garden/presentation/GardenController.java
+++ src/main/java/com/example/cp_main_be/domain/garden/garden/presentation/GardenController.java
@@ -54,14 +54,6 @@ public class GardenController {
     return ResponseEntity.ok(ApiResponse.success(null));
   }
 
-  @Operation(summary = "텃밭 슬롯 확장", description = "텃밭 슬롯 확장")
-  @PutMapping("/slots/unlock")
-  public ResponseEntity<ApiResponse<Void>> unlockGarden() {
-    User currentUser = userService.getCurrentUser();
-    gardenService.unlockNextGarden(currentUser.getId());
-    return ResponseEntity.ok(ApiResponse.success(null));
-  }
-
   @Operation(summary = "텃밭 슬롯 해금", description = "해금 버튼 클릭 시 호출")
   @PostMapping("/unlock")
   public ResponseEntity<Void> unlockGarden(@AuthenticationPrincipal User user) {
