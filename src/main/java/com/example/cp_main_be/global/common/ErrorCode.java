f23acb0a986dad56f7caded7ca672bc8ee9ad459
c5ln <70241022+c5ln@users.noreply.github.com>
2025-08-28 00:38:55 +0900

    refactor/물주기 햇빛주기 에러 수정 (#142)
    * refactor/물주기 햇빛주기 에러 수정
    
    * refactor/물주기 햇빛주기 에러 수정

--- src/main/java/com/example/cp_main_be/global/common/ErrorCode.java
+++ src/main/java/com/example/cp_main_be/global/common/ErrorCode.java
@@ -10,11 +10,11 @@ public enum ErrorCode {
   // === 4xx Client Errors ===
   // 400 Bad Request
   INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "E40001", "유효하지 않은 토큰입니다."),
-  WATERING_COOL_DOWN(HttpStatus.BAD_REQUEST, "E40002", "아직 물을 줄 수 없습니다. 8시간이 지나야 가능합니다."),
+  WATERING_COOL_DOWN(HttpStatus.ACCEPTED, "E20002", "아직 물을 줄 수 없습니다."),
   FRIEND_WATERING_LIMIT_EXCEEDED(
       HttpStatus.BAD_REQUEST, "E40003", "오늘은 다른 사람의 정원에 더 이상 물을 줄 수 없습니다."),
-  ALREADY_WATERED_GARDEN(HttpStatus.BAD_REQUEST, "E40004", "이 정원에는 오늘 이미 물을 주었습니다."),
-  SUNLIGHT_COOL_DOWN(HttpStatus.BAD_REQUEST, "E40005", "오늘은 이미 햇빛을 주었습니다."),
+  ALREADY_WATERED_GARDEN(HttpStatus.ACCEPTED, "E20002", "이 정원에는 오늘 이미 물을 주었습니다."),
+  SUNLIGHT_COOL_DOWN(HttpStatus.ACCEPTED, "E20002", "오늘은 이미 햇빛을 주었습니다."),
   GARDEN_SLOT_MAXED_OUT(
       HttpStatus.BAD_REQUEST, "E40006", "더 이상 텃밭을 추가할 수 없습니다."), // 최종 3개 도달 시 사용 가능
   INVALID_FILE(HttpStatus.BAD_REQUEST, "E40007", "적절하지 않은 파일 내용/포맷입니다."),
