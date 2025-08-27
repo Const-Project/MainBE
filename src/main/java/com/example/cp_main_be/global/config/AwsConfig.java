7f01c3b699ea2f8b0b8a5bf887295fa1c8a2fa7c
lejuho <zedd1324@gmail.com>
2025-08-27 17:37:24 +0900

    문제 해결

--- src/main/java/com/example/cp_main_be/global/config/AwsConfig.java
+++ src/main/java/com/example/cp_main_be/global/config/AwsConfig.java
@@ -1,49 +1,20 @@
-// AwsConfig.java 또는 R2Config.java
 package com.example.cp_main_be.global.config;
 
-import java.net.URI;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
-import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
 @Configuration
-public class AwsConfig { // 파일 이름은 그대로 두셔도 됩니다.
-
-  @Value("${cloudflare.r2.account-id}")
-  private String accountId;
-
-  @Value("${cloudflare.r2.access-key}")
-  private String accessKey;
-
-  @Value("${cloudflare.r2.secret-key}")
-  private String secretKey;
+public class AwsConfig {
 
   @Bean
   public S3Client s3Client() {
-    // R2 접속을 위한 엔드포인트 URL 생성
-    String endpoint = String.format("https://%s.r2.cloudflarestorage.com", accountId);
-
-    String endpointLiteral = "https://" + accountId + ".r2.cloudflarestorage.com";
-
-    // 디버깅용 로그 추가
-    System.out.println("R2 Endpoint: " + endpointLiteral);
-    System.out.println("Access Key: " + accessKey);
-    System.out.println("Account ID: " + accountId);
-
-    // R2 인증 정보 설정 (SDK v2 방식)
-    AwsBasicCredentials credentials = AwsBasicCredentials.create(accessKey, secretKey);
-    StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(credentials);
-
-    // S3 클라이언트 빌드 (SDK v2 방식)
+    // AWS SDK가 EC2 환경임을 자동으로 감지하고,
+    // 연결된 IAM 역할(ec2-aws)의 권한을 자동으로 사용합니다.
+    // 우리는 리전만 지정해주면 됩니다.
     return S3Client.builder()
-        .endpointOverride(URI.create(endpoint)) // R2 엔드포인트 지정
-        .region(Region.of("auto")) // R2는 리전이 없으므로 'auto'
-        .credentialsProvider(credentialsProvider)
-        .forcePathStyle(true)
+        .region(Region.AP_NORTHEAST_2) // 서울 리전
         .build();
   }
 }
