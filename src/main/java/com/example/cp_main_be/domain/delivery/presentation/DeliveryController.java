5822778ae48d2560b407c0a7157e01097117ef74
lejuho <zedd1324@gmail.com>
2025-08-28 16:58:01 +0900

    로직 수정

--- src/main/java/com/example/cp_main_be/domain/delivery/presentation/DeliveryController.java
+++ src/main/java/com/example/cp_main_be/domain/delivery/presentation/DeliveryController.java
@@ -13,7 +13,6 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -27,14 +26,10 @@ public class DeliveryController {
   @Operation(summary = "씨앗 배송 정보를 입력받습니다")
   @PostMapping("/seeds")
   public ResponseEntity<ApiResponse<Void>> requestDelivery(
-      @AuthenticationPrincipal UserDetails userDetails, // 현재 로그인한 사용자 정보
+      @AuthenticationPrincipal User user, // 현재 로그인한 사용자 정보
       @Valid @RequestBody DeliveryRequest deliveryRequest) {
 
-    // userDetails에서 사용자 ID를 추출하여 서비스에 전달
-    // Long userId = ((CustomUserDetails) userDetails).getId(); // UserDetails 구현체에 맞게 수정
-    Long userId = 1L; // 임시 ID, 실제로는 위와 같이 인증 정보에서 가져와야 합니다.
-
-    deliveryService.createDeliveryRequest(userId, deliveryRequest);
+    deliveryService.createDeliveryRequest(user.getId(), deliveryRequest);
 
     return ResponseEntity.ok(ApiResponse.success(null));
   }
