4ee29bea616963524680c226172e6543ee78586f
lejuho <zedd1324@gmail.com>
2025-08-28 15:55:38 +0900

    배송 관련 수정

--- src/main/java/com/example/cp_main_be/domain/delivery/presentation/DeliveryController.java
+++ src/main/java/com/example/cp_main_be/domain/delivery/presentation/DeliveryController.java
@@ -2,7 +2,9 @@ package com.example.cp_main_be.domain.delivery.presentation;
 
 import com.example.cp_main_be.domain.delivery.dto.request.DeliveryRequest;
 import com.example.cp_main_be.domain.delivery.dto.response.DeliveryPlantResponse;
+import com.example.cp_main_be.domain.delivery.dto.response.DeliveryResponse;
 import com.example.cp_main_be.domain.delivery.service.DeliveryService;
+import com.example.cp_main_be.domain.member.user.domain.User;
 import com.example.cp_main_be.global.common.ApiResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -42,5 +44,12 @@ public class DeliveryController {
   public ResponseEntity<ApiResponse<List<DeliveryPlantResponse>>> getDeliveryPlantList() {
     List<DeliveryPlantResponse> deliveryPlantList = deliveryService.getDeliveryPlantList();
     return ResponseEntity.ok(ApiResponse.success(deliveryPlantList));
+  }
+
+  @Operation(summary = "배송 정보 조회", description = "지금까지 한 나의 모든 배송 정보를 조회합니다.")
+  @GetMapping
+  public ResponseEntity<ApiResponse<List<DeliveryResponse>>> getMyDelivery(
+      @AuthenticationPrincipal User user) {
+    return ResponseEntity.ok(ApiResponse.success(deliveryService.getMyDelivery(user)));
   }
 }
